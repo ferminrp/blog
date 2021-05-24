1d442541bf612e5a70e4b71a7868fa576fb4a678
ferminrp <frodriguezpenelas@gmail.com>
2021-05-24 16:56:06 -0300

    vault backup: 2021-05-24 16:56:06

--- publish.js
+++ publish.js
@@ -1,8 +1,16 @@
+// token que nos da botfather
 const token = '1829060072:AAHAqOibf9uHLq6-eB4lF1gm8TB8vE7a3G8'
+
+// username del bot que creamos
 const bot_username = '@que_toca_tomar_bot'
+
 const master_id = 111111111
 // tks https://github.com/kokororin/hitokoto
-let bebidas = ["Birrita", "vino", "campari"]
+
+// Array de opciones
+let bebidas = ["🍺 Birrita", "🍷 Vino", "🍹 Campari", "🤘 Fernecito", "🥃 Whiskicito "]
+
+// Esta funcion mezcla el array de bebidas para dejarlo en un orden aleatorio
 function shuffle(array, seed) {
   let currentIndex = array.length, temporaryValue, randomIndex
   seed = seed || 1
@@ -19,33 +27,50 @@ function shuffle(array, seed) {
   }
   return array
 }
+
+// Definimos un event listener que escuche a nuevas solcitudes del api
 addEventListener('fetch', event => {
+  // Cuando se de un event, vamos a llamar a la funcion handleRequest pasandole el parametro request del event
   event.respondWith(handleRequest(event.request))
 })
+
+// Esta funcion recibe el request y va a devolver la respuesta
 async function handleRequest(request) {
+  // Manipula las request de tipo post
   if (request.method == 'POST') {
+    // Transformo la request en un json
     let data = await request.json()
+    // Chequeo si la request trae un mensaje
     if (data.message !== undefined) {
+      // Si la request trae un mensaje se lo pasa a la funcin handlemessage
       handlemessage(data.message)
     } else if (data.inline_query !== undefined) {
+      // Si la request no trae un mensaje pero trae inline_query se lo pasa a la funcion handleinline
       handleinline(data.inline_query)
     }
   }
+  // Devuelvo status ok 200 a la consulta del endpoint
   return new Response('ok', { status: 200 })
 }
+
+// Esta funcion recibe el mensaje del request si lo hay
 async function handlemessage(d) {
-  let chat_id = d.chat.id
-  let text = d.text || ''
-  let otext = text.split(' ')
+  let chat_id = d.chat.id                                             // id de la conversacion
+  let text = d.text || ''                                             // mensaje recibido
+  let otext = text.split(' ')                                         // separo el mensaje en palabras
+  // Si el mensaje arranca con una "/"
   if (text[0] == '/') {
+    // Elimino la barra y el nombre del bot de las palabras del mensaje
     otext[0] = otext[0].replace('/', '').replace(bot_username, '')
     switch (otext[0]) {
+      // Si el comando recibido fue "/start" devuelvo la bienvenida
       case 'start':
         await tg(token, 'sendmessage', {
           chat_id: chat_id,
-          text: 'Hola, bienvenido al bot que te dice que tomar. Envia "?" para que te sugiera una bebida.'
+          text: 'Hola, bienvenido al bot que te dice que tomar. Envia "/?" para que te sugiera una bebida.'
         })
         break
+      // Si el comando recibido fue "/?" devuelvo lo que traiga la funcion hitoko
       case '?':
         let h = hitokoto()
         await tg(token, 'sendmessage', {
@@ -93,9 +118,13 @@ async function handleinline(d) {
     next_offset: offset.join('|')
   })
 }
+
+// esta funcion devuelve un item aleatoreo del array de ebidas
 function hitokoto() {
   return bebidas[Math.floor(Math.random() * bebidas.length)]
 }
+
+// Esta funcion manda un mensaje a traves del bot
 async function tg(token, type, data, n = true) {
   try {
     let t = await fetch('https://api.telegram.org/bot' + token + '/' + type, {
